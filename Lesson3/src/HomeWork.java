54e63e361261d74b705918fe3e95a6bf99f66f90
ARESKAN2108 <areskan@mail.ru>
2021-06-26 17:28:19 +0300

    Lesson3, HomeWork Сделал домашку №3!

---
+++ Lesson3/src/HomeWork.java
@@ -0,0 +1,228 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+        //Некоторые тесты для проверки задач. Можно также написать свои тесты.
+        printArray();
+        System.out.println(operation(1));
+        System.out.println(operation(0));
+        System.out.println(calculateCountOfOddElementsInMatrix(new int[]{1, 2, 3, 4, 5, 6}));
+        calculateSumOfDiagonalElements();
+        countDevs(103);
+        foobar(6);
+        foobar(10);
+        foobar(15);
+        printMatrix();
+        printPrimeNumbers();
+    }
+
+
+    /**
+     * Необходимо прочитать с консоли значение числа типа int,
+     * далее создать одноменрый массив типа int размера прочитаного с консоли
+     * далее заполнить массив случайными значениями
+     * далее вывести массив на консоль
+     */
+    private static void printArray() {
+        // Можно еще через Random сделать.Я сделал через класс Math.
+        System.out.println("Введите число:");
+        Scanner scanner = new Scanner(System.in);
+        int sizeArray = scanner.nextInt();
+        int[] array = new int[sizeArray];
+        if (sizeArray == 0) {
+            System.out.println("Array is empty");
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (int) (Math.random() * 100);
+            }
+            System.out.println(Arrays.toString(array));
+        }
+    }
+
+    /**
+     * Метод должен выполнять некоторую операцию с int "number" в зависимости от некоторых условий:
+     * - if number положительное число, то необходимо number увеличить на 1
+     * - if number отрицательное - уменьшить на 2
+     * - if number равно 0 , то замените значение number на 10
+     * вернуть number после выполнения операций
+     */
+
+    public static int operation(int number) {
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
+        if (number > 0) {
+            number++;
+        }
+        if (number < 0) {
+            number = number - 2;
+        }
+        if (number == 0) {
+            number = 10;
+        }
+        return number;
+    }
+
+    /**
+     * На вход приходит массив целых чисел типа int
+     * Необходимо найти количество нечетных элементов в массиве и вернуть значение в метод main,
+     * в котором это значение распечатается на консоль.
+     */
+
+    public static int calculateCountOfOddElementsInMatrix(int[] ints) {
+        int count = 0;
+        if (ints.length == 0) {
+            System.out.println("Array is empty");
+        } else {
+            for (int anInt : ints) {
+                if (anInt % 2 == 1 || anInt % 2 == -1 || anInt == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * На вход приходит число.
+     * Вывести в консоль фразу из разряда "_COUNT_ программистов",
+     * заменить _COUNT_ на число которое пришло на вход в метод и заменить окончание в слове "программистов" на
+     * уместное с точки зрения русского языка.
+     * Пример: 1 программист, 42 программиста, 50 программистов
+     *
+     * @param count - количество программистов
+     */
+
+    // не нравится стиль кода, но по-другому не смог придумать пока что
+    public static void countDevs(int count) {
+        int devsCount = count % 100;
+        if (devsCount < 11 || devsCount > 14) {
+            devsCount = count % 10;
+            if (devsCount == 1) {
+                System.out.println(count + " программист");
+            } else if (devsCount >= 2 && devsCount <= 4) {
+                System.out.println(count + " программиста");
+            } else System.out.println(count + " программистов");
+        }
+        if (devsCount == 11 || devsCount == 12 || devsCount == 13 || devsCount == 14) {
+            System.out.println(count + " программистов");
+        }
+    }
+
+    /**
+     * Метод должен выводить разные строки в консоли в зависимости от некоторых условий:
+     * - если остаток от деления на 3 равен нулю - выведите "foo" (example of number - 6)
+     * - если остаток от деления на 5 равен нулю - вывести "bar" (example of number - 10)
+     * - если остаток от деления на 3 и 5 равен нулю 0 ,то вывести "foobar" (example of number - 15)
+     */
+
+    public static void foobar(int number) {
+        if (number % 3 == 0 & number % 5 == 0) {
+            System.out.println("foobar");
+        } else if (number % 3 == 0) {
+            System.out.println("foo");
+        } else if (number % 5 == 0) {
+            System.out.println("bar");
+        } else {
+            System.out.println("Let`s go again");
+        }
+    }
+
+    /**
+     * заполнить рандомно 2-х мерный массив и посчитать сумму элементов на диагонали
+     */
+
+    public static void calculateSumOfDiagonalElements() {
+        int[][] array = new int[3][3];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 10);
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (i == j) {
+                    sum += array[i][j];
+                }
+            }
+        }
+        System.out.println("Сумма главной диагонали " + sum);
+
+    }
+
+    /**
+     * Шаги по реализации:
+     * - Прочитать два int из консоли
+     * - Создайте двумерный массив int (используйте целые числа, которые вы читаете по высоте и ширине консоли)
+     * - Заполнить массив случайными значениями (до 100)
+     * - Вывести в консоль матрицу заданного размера, но:
+     * - Если остаток от деления элемента массива на 3 равен нулю - выведите знак "+" вместо значения элемента массива.
+     * - Если остаток от деления элемента массива на 7 равен нулю - выведите знак "-" вместо значения элемента массива.
+     * - В противном случае выведите "*"
+     * <p>
+     * Example:
+     * - Значения с консоли - 2 и 3
+     * - Массив будет выглядеть так (значения будут разными, потому что он случайный)
+     * 6 11 123
+     * 1 14 21
+     * - Для этого значения вывод в консоли должен быть:
+     * <p>
+     * + * *
+     * * - +
+     * <p>
+     * Обратите внимание, что 21% 3 == 0 и 21% 7 = 0, но выводить надо не +-, а +
+     */
+
+    public static void printMatrix() {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int heigh = scanner.nextInt();
+        int width = scanner.nextInt();
+        int[][] array = new int[heigh][width];
+        String[][] arrayStrings = new String[heigh][width];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = random.nextInt(100);
+                if (array[i][j] % 7 == 0 & array[i][j] % 3 == 0) {
+                    arrayStrings[i][j] = "+";
+                } else if (array[i][j] % 3 == 0) {
+                    arrayStrings[i][j] = "+";
+                } else if (array[i][j] % 7 == 0) {
+                    arrayStrings[i][j] = "-";
+                } else arrayStrings[i][j] = "*";
+                System.out.print(arrayStrings[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    /**
+     * Задача со звездочкой!
+     * Метод должен печатать все простые числа <1000
+     * что такое просто число (https://www.webmath.ru/poleznoe/formules_18_5.php)
+     */
+
+    public static void printPrimeNumbers() {
+        for (int i = 2; i < 1000; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+
