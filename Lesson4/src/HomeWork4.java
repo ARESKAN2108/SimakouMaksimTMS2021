d43e90ecc50f250a1b6fab65d4f5fcb35b5fb155
ARESKAN2108 <areskan@mail.ru>
2021-06-30 03:06:03 +0300

    Домашка 4. Буду по тихоньку добавлять методы.

--- Lesson4/src/HomeWork4.java
+++ Lesson4/src/HomeWork4.java
@@ -1,6 +1,7 @@
 public class HomeWork4 {
     public static void main(String[] args) {
         amoeba();
+        System.out.println("Результат вычисления умножения = " + summ(5, 6));
     }
 
     // 1) Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
@@ -21,5 +22,12 @@ public class HomeWork4 {
             }
         }
         System.out.println("За сутки будет 256 амеб");
+    }
+
+    //2) Напишите реализацию метода summ(int a, int b), вычисляющий a*b, не пользуясь операцией
+    //умножения, где a и b целые числа, вызовите метод summ  в методе main и распечатайте на консоль.
+
+    private static int summ(int a, int b) {
+        return Math.multiplyExact(a, b);
     }
 }
