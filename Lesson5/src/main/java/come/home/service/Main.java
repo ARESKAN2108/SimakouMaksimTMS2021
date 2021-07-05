986bc442d10089b91416c8e4724cb853b8c05513
ARESKAN2108 <areskan@mail.ru>
2021-07-05 18:57:41 +0300

    Добавил for в методе main для прокрутки методы on.

--- Lesson5/src/main/java/come/home/service/Main.java
+++ Lesson5/src/main/java/come/home/service/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Computer computer = new Computer("Intel", 16, 1000, 3);
         System.out.println(computer.getInfo());
-        computer.on();
+        for (int i = 6; i >= 0; i--) {
+            computer.on();
+        }
 
     }
 }
