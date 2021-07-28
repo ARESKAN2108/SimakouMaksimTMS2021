7b97135623f50c071493efb585bd5498b4b4f71c
ARESKAN2108 <maxareskan@gmail.com>
2021-07-28 19:22:40 +0300

    refactor

--- Lesson12/src/main/java/com/tms/exception/homework2/Authorization.java
+++ Lesson12/src/main/java/com/tms/exception/homework2/Authorization.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Authorization {
 
-    private static final String CORRECT_LOGIN = "Логин должен быть из латинских букв, цифр и знака подчеркивания.\nВведите повторно";
-    private static final String CORRECT_PASSWORD = "Пароль должен быть из латинских букв, цифр и знака подчеркивания.\nВведите повторно";
+    private static final String CORRECT_LOGIN = "Логин должен быть из латинских букв, цифр и знака подчеркивания.\nЛибо вы ввели больше 20 символов\nВведите повторно";
+    private static final String CORRECT_PASSWORD = "Пароль должен быть из латинских букв, цифр и знака подчеркивания.\nЛибо вы ввели больше 20 символов\nВведите повторно";
     private static final String PASSWORD_MISMATCH = "Пароли не совпадают. Введите пароль заново: ";
 
     public static void checkAuthorization(String login, String password, String passwordConfirm) {
         Scanner scanner = new Scanner(System.in);
         try {
-            if (!login.matches("_*\\d*?[A-Za-z_]+\\d*_?") && !(login.length() > 20)) {
+            if (!login.matches("_*\\d*?[A-Za-z_]+\\d*_?") && !(login.length() >= 20)) {
                 throw new WrongLoginException(CORRECT_LOGIN);
             } else {
-                if (!password.matches("_*\\d*?[A-Za-z_]+\\d*_?") && !(password.length() > 20)) {
+                if (!password.matches("_*\\d*?[A-Za-z_]+\\d*_?") && !(password.length() >= 20)) {
                     throw new WrongPasswordException(CORRECT_PASSWORD);
                 } else {
                     System.out.println("Подтвердите пароль:");
