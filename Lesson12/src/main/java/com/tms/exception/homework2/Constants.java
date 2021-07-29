bbcbb702515b5c44db79b4789841fba01321529e
ARESKAN2108 <maxareskan@gmail.com>
2021-07-30 02:29:34 +0300

    пока что так сделал. мнение о реализации описал в майне

--- Lesson12/src/main/java/com/tms/exception/homework2/Constants.java
+++ Lesson12/src/main/java/com/tms/exception/homework2/Constants.java
@@ -10,5 +10,4 @@ public final class Constants {
     public static final String CORRECT_PASSWORD = "Пароль может содержать латинские буквы, цифры либо, знак: \"_\".\nНе менее 8-ми символов и не более 20-ти.\nВведите повторно:";
     public static final String PASSWORD_MISMATCH = "Пароли не совпадают. Введите пароль заново:";
     public final static Scanner scanner = new Scanner(System.in);
-
 }
